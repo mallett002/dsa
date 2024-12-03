ecf2fce0691c05762b39b768f4cf31a612ad83a6
William Mallett <mallett002@gmail.com>
2024-12-03 08:30:00 -0600

    triangle_nums finish

--- two-pointers/triangle_nums.py
+++ two-pointers/triangle_nums.py
@@ -2,24 +2,21 @@
 # The triplets do not need to be unique.
 def triangle_numbers(nums):
     nums.sort()
-
     count = 0
 
-    for i in range(len(nums) - 2):
-        left = i + 1
-        right = len(nums) - 1
+    # loop backwards, from end to 3rd from beginning (index 2) - leave room for left and right pointers
+    # [2, 9, 10, 12]
+    for i in range(len(nums) - 1, 1, -1):
+        left = 0
+        right = i - 1
 
         while left < right:
-            # want a + b > c (valid triangle)
-            # a + b < c     --> 
-            # move left up (try and get bigger number)
-            if nums[i] + nums[left] < nums[right]:
-                left += 1
+            if nums[left] + nums[right] > nums[i]:
+                count += right - left # all nums from left to right are valid
+                right -= 1 # decrement right to see if this one also works
+            else:
+                left += 1 # increment left
 
-            if nums[i] + nums[left] > nums[right]:
-                count += 1
-                left += 1
-    
     return count
 
 # i: 4
@@ -37,36 +34,3 @@ def triangle_numbers(nums):
 # 4 + 11 > 18: no; left += 1  
 # 6 + 11 > 18: yes; count += 2; right -= 1;
 # 6 + 9 > 18: no; left += 1; left == right; break
-
-# 4 + 11 > 15: yes... 
-
-def triangle_numbers2(nums):
-    nums.sort()
-
-    count = 0
-
-    start = len(nums) - 1
-    stop = 1
-    step = -1
-
-    for i in range(start, stop, step):
-        left = 0
-        right = i - 1
-
-        while left < right:
-            if nums[left] + nums[right] > nums[i]: # found triangle
-                count += right - left # all nums in-between right and left will also be triangles (bc sorted)
-                right -= 1 # move right back (see if next # is big enough to create triangle)
-            
-            else: # no triangle
-                # all nums btw left and right are also not triangles
-                # [4, 6, 13]  
-                left += 1 # move left up to try and find a bigger number to use
-    
-    return count
-
-
-input = [11, 4, 9, 6, 15, 18]
-# [4, 6, 9, 11, 15, 18]
-#  0  1  2   3   4   5
-print(triangle_numbers2(input))
