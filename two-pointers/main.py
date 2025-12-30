1ebc9be364f6f8f752ebd7e792935deecccd8fa9
mallett002 <mallett002@gmail.com>
2025-12-30 07:16:52 -0600

    :broom:

--- two-pointers/main.py
+++ two-pointers/main.py
@@ -1,24 +1,20 @@
 # nums = [1,3,4,6,8,10,13], target = 13
 def two_sum(nums, target):
-    # pointers on the outside indexes
     left = 0
     right = len(nums) - 1
 
-    # if curr sum > target: all with current "right" will be greater. move "right" back one
-    # if curr < target: all with left will be smaller. move "left" up one
-
-    while left < right: # while they haven't met
+    while left < right:
         sum = nums[left] + nums[right]
 
         if sum == target:
             return True
 
         if sum > target:
-            right -= right;
-            continue;
-        
+            right -= right
+            continue
+
         if sum < target:
-            left += left;
-            continue;
-    
+            left += left
+            continue
+
     return False
