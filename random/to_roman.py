3121e394f2750522e7d8f7caa7fda34ba342f6ad
mallett002 <mallett002@gmail.com>
2026-01-16 06:36:17 -0600

    :broom:

--- random/to_roman.py
+++ random/to_roman.py
@@ -30,19 +30,9 @@ print(divmod(395, 300))  # (12, 1)
 # This one was the first attempt:
 def to_roman(num):
     lookup = {
-        'M': 1000,
-        'CM': 900,
-        'D': 500,
-        'CD': 400,
-        'C': 100,
-        'XC': 90,
-        'L': 50,
-        'XL': 40,
-        'X': 10,
-        'IX': 9,
-        'V': 5,
-        'IV': 4,
-        'I': 1
+        'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100,
+        'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9,
+        'V': 5, 'IV': 4, 'I': 1
     }
 
     romans = []
